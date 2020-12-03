1c4ebd0d5694b7acb4df5f8bc47b54dde44b12f9
Imature <benedictusmariakartika@gmail.com>
2020-12-03 15:49:29 +0700

    add adapter rv

---
+++ app/src/main/java/com/example/pas_11rpl1_beni/MainMenu.java
@@ -0,0 +1,14 @@
+package com.example.pas_11rpl1_beni;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
